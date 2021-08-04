4c7dfa8686a313542dfcc02ce04b799cbb7cd78d
josueagurto <87269329+josueagurto@users.noreply.github.com>
2021-08-04 13:39:41 -0500

    correo electronico

--- resources/views/modelo/plantilla.blade.php
+++ resources/views/modelo/plantilla.blade.php
@@ -6,8 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- favico --}}
     <title>@yield('title')</title>
+    <style>
+        .active{
+            color:brown;
+            font-weight: bold;
+            background-color: beige;
+        }
+    </style>
 </head>
 <body>
+    @include('modelo.partials.header')
+
     @yield('content')
+    @include('modelo.partials.footer')
 </body>
 </html>
